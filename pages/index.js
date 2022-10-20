4c68dda3d2093e94ff5a3cf62590b9f75b64d9da
Regina Sherman <reginasherman@Reginas-MacBook-Pro.local>
2022-10-19 21:24:49 -0500

    try again

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,6 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Contact from '../components/Contact'
 
-import IRFoundation from '../components/Projects/IRFoundation'
-import ApplePieDay from '../components/Projects/ApplePieDay'
-import CompanyEvent from '../components/Projects/CompanyEvent'
-import GreatIowaHomes from '../components/Projects/GreatIowaHomes'
-import IRAgent from '../components/Projects/IRAgent'
-import RingerGolf from '../components/Projects/RingerGolf'
-import SchoolSupplyDrive from '../components/Projects/SchoolSupplyDrive'
-import UnitedWay from '../components/Projects/UnitedWay'
-
 import Projects from '../components/Projects'
 
 export default function Home () {
