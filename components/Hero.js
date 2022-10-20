4c6d533d0a2b3e308f0cd974f5dec03609604703
Regina Sherman <reginasherman@Reginas-MacBook-Pro.local>
2022-10-20 13:34:54 -0500

    updated hero, most of responsive layout done

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 import '@fontsource/montserrat-alternates'
 
-import bg from '../public/at-designs-hero.jpg'
+import bg from '../public/pexels-kseniya-lapteva-9176101.jpg'
 import otter from '../public/Otter White (1).svg'
 
 const Hero = () => {
@@ -14,7 +14,7 @@ const Hero = () => {
           className='hero__container-img'
           style={{
             backgroundImage: `url(${bg.src})`,
-            backgroundColor: 'rgba(0, 0, 0, 0.65)',
+            backgroundColor: 'rgba(0, 0, 0, 0.45)',
             backgroundBlendMode: 'multiply',
             backgroundSize: 'cover',
             width: '100vw',
@@ -23,7 +23,7 @@ const Hero = () => {
           }}
         ></div>
         <div className='hero__container-txt'>
-            <Image src={otter} width={200} height={200}/>
+            <Image className='otter' src={otter} width={200} height={200}/>
           <h1><strong className='strong'>AmaraUtter</strong>Designs</h1>
         </div>
       </div>
